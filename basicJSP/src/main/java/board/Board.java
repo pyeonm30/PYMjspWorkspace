594727137cebea2aec0272624c9b5c6b9d473363
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2024-02-01 10:17:01 +0900

    .
    .

--- basicJSP/src/main/java/board/Board.java
+++ basicJSP/src/main/java/board/Board.java
@@ -1,5 +1,57 @@
 package board;
 
 public class Board {
-
+    private int no; // 게시글 번호
+    private String writer; //작성자
+    private String subject;//제목
+    private String contents;//내용
+    private String regDate;//작성일
+  
+	public Board(int no, String writer,String regDate, String subject, String contents) {
+		super();
+		this.no = no;
+		this.writer = writer;
+		this.subject = subject;
+		this.contents = contents;
+		this.regDate = regDate;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	@Override
+	public String toString() {
+		return "Board [no=" + no + ", writer=" + writer + ", subject=" + subject + ", contents=" + contents
+				+ ", regDate=" + regDate + "]";
+	}
+    
+	
+    
+    
 }
--- basicJSP/src/main/webapp/08_basic_board/_00_main.jsp
+++ basicJSP/src/main/webapp/08_basic_board/_00_main.jsp
@@ -7,6 +7,16 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	<h1>게시판</h1>
+	
+	<h3><a href="_01_boardList.jsp">전체 게시글 확인하기</a></h3>
+	<h3><a href="_02_addDummyPro.jsp">더미 게시글 추가하기</a></h3>
+	<h3><a href="_03_deleteAllBoard.jsp">전체 게시글 삭제하기</a></h3>
+	<h3><a href="_04_addBoard.jsp">게시글 추가하기</a></h3>
+	
+	<hr>
+	
+	<h3><a href="_07_boardListPaging.jsp">페이징 게시판</a></h3>
+	<h3><a href="_08_boardListPaging2.jsp">페이징 게시판2</a></h3>
 </body>
 </html>
